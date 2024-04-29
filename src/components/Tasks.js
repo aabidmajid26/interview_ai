bac3a4df896b13b8e33ae8653aa0fc0317c91776
Aabid Majid <aabidmajid26@gmail.com>
2024-04-29 19:10:29 +0530

    Revert "sorting tasks done."
    This reverts commit 68a006f9b45f6698d95ee64311708042c331a035.

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { addTask, deleteTask, updateTask } from "../store/taskSlice";
 import Task from "./Task";
-import { sortTasks } from "../utils/sortTasks";
 
 const Tasks = () => {
   const [task, setTask] = useState({
@@ -15,8 +14,6 @@ const Tasks = () => {
     key: null,
   });
   const tasks = useSelector((state) => state.tasks);
-
-  const sortedTasks = sortTasks(tasks.tasks, "priority");
 
   const dispatch = useDispatch();
 
@@ -66,7 +63,7 @@ const Tasks = () => {
       <div>
         <table>
           <tbody>
-            {sortedTasks.map((task, idx) => {
+            {Object.values(tasks.tasks).map((task, idx) => {
               return <Task key={task.name} task={task} idx={idx} />;
             })}
           </tbody>
